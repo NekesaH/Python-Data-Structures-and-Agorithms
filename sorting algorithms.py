711f986b56f69efa3f73a557f6ded4423ff530bd
NekesaH <nyongesacharity13@gmail.com>
2022-07-14 14:46:45 +0300

    insertion sort

---
+++ sorting algorithms.py
@@ -0,0 +1,94 @@
+#merge sort algorithms
+#Time complexity
+# Space Complexity
+
+ 
+
+
+
+
+
+#insertion sort
+# space O(1)
+# Time 0(n2)
+
+# def insertion_sort(array):
+#     i = j
+#     for i in range(1, len(array)):
+#         while j > 0 and array[j] < array[j-1]:
+#             swap(i, j-1, array)
+#             j -= 1
+
+# def swap(array, i , j):
+#     array[i], array[j] = array[j], array[i]
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
