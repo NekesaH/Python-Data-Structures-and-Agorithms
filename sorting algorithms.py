c9ce4511664475478052c22febd865f25a21b2ea
NekesaH <nyongesacharity13@gmail.com>
2022-07-27 08:51:21 +0300

    merge sort

--- sorting algorithms.py
+++ sorting algorithms.py
@@ -3,50 +3,56 @@
 # Space Complexity
 # Time = O(N log(N)) Space = O(N log(N))
 
-#from heapq import merge
 
 
-
-def merge_sort(arr):
-    if len(arr) > 1:
-        left_arr = arr[:len(arr)//2]
-        right_arr = arr[len(arr)//2:]
-  
-    #recursion
+#Given the head of a linkedlist return the list after srting it in ascending order
+def sortList(self, head):
+    if head is None or head.next is None:
+        return head
+    slow, fast = head, head.next
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next
     
-    merge_sort(left_arr)
-    merge_sort(right_arr)
 
-    i=0 # left_array index
-    j=0 #right_array index
-    k = 0 #merged array index
 
-    while i < len(left_arr) and j < len(right_arr):
-        if left_arr[i] < right_arr[j]:
-            arr[k] = left_arr[i]
-            i += 1
+# def merge_sort(arr):
+#     if len(arr) > 1:
+#         left_arr = arr[:len(arr)//2]
+#         right_arr = arr[len(arr)//2:]
+  
+#     #recursion
+    
+#     merge_sort(left_arr)
+#     merge_sort(right_arr)
+
+#     i=0 # left_array index
+#     j=0 #right_array index
+#     k = 0 #merged array index
+
+#     while i < len(left_arr) and j < len(right_arr):
+#         if left_arr[i] < right_arr[j]:
+#             arr[k] = left_arr[i]
+#             i += 1
             
-        else:
-            arr[k] = right_arr[j]
-            j += 1
-        k += 1 
+#         else:
+#             arr[k] = right_arr[j]
+#             j += 1
+#         k += 1 
 
-    while i < len(left_arr):
-        arr[k] = left_arr[i]
-        i += 1
-        k += 1
-    while j < len(right_arr):
-        arr[k] = right_arr[j]
-        j += 1
-        k += 1
-
-
-arr_test = [2, 3, 5, 1,7, 4, 4, 4, 2, 6, 0]
-merge_sort(arr_test)
-print(arr_test)
+#     while i < len(left_arr):
+#         arr[k] = left_arr[i]
+#         i += 1
+#         k += 1
+#     while j < len(right_arr):
+#         arr[k] = right_arr[j]
+#         j += 1
+#         k += 1
 
 
-
+# arr_test = [2, 3, 5, 1,7, 4, 4, 4, 2, 6, 0]
+# merge_sort(arr_test)
+# print(arr_test)
 
 
 
